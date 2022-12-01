f5c8ee94510934c1738faf8de9d628429ae61fc5
Jayjader <jayjader@github.com>
2022-12-01 14:23:09 +0100

    day 1, part 1 - solution

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+extern crate core;
+
+use std::fs;
+/// solve problem for day 1
+fn day1(input: &str) -> usize {
+    /// Takes a list of inventories, separated by blank lies, of calories, separated by newlines, as input.
+    /// Returns the sum of the one containing the most calories.
+    fn part1(input: &str) -> usize {
+        input
+            .split("\n\n")
+            .map(|inventory_as_string| {
+                inventory_as_string
+                    .split('\n')
+                    .map(|calories| calories.parse::<usize>().unwrap_or(0))
+                    .sum::<usize>()
+            })
+            .max()
+            .unwrap()
+    }
+
+    part1(input)
+}
+
+/// passes problem input to solver for the given day
+fn main() {
+    let contents = fs::read_to_string("./input")
+        .expect("where's the input file? didn't find it at './input'.");
+    let solution = day1(&contents);
+    println!("Solution: {}", solution)
+}
