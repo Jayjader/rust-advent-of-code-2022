0ae28122242c208ba656e39a73b0995fb0c010b2
Jayjader <jayjader@github.com>
2022-12-05 11:32:12 +0100

    day 5, part 1 - solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate core;
 
-use std::collections::HashSet;
+use regex::Regex;
+use std::collections::{HashSet, VecDeque};
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::iter::{FlatMap, Map};
@@ -321,15 +322,53 @@ fn day4(input: &str, part: Part) -> usize {
 }
 
 /// solves problem for day 5
-fn day5(input: &str, part: Part) -> usize {
-    0
+fn day5(input: &str, part: Part) -> String {
+    let (drawing, commands) = input.split_once("\n\n").unwrap();
+    let newline_index = drawing.find('\n').unwrap();
+    // a drawing with n stacks has 4n-1 characters per line:
+    // 3 per stack (3n) + in between the stacks (n-1)
+    let number_of_stacks = (newline_index + 1) / 4;
+    let mut stacks = Vec::with_capacity(number_of_stacks);
+    stacks.resize_with(number_of_stacks, VecDeque::<char>::new);
+    // skip the last line as we can just infer the stack indices
+    let end_of_stacks = drawing.rfind('\n').unwrap();
+    for line in drawing[0..end_of_stacks].lines() {
+        for (index, c) in line.chars().enumerate() {
+            // [S] [D]     [W]     [W]     [H] [Q]
+            //  |   |       |
+            // 0123012301230123...
+            //  ^   ^       ^
+            if index % 4 == 1 && c.is_alphabetic() {
+                stacks[index / 4].push_back(c)
+            }
+        }
+    }
+    // parse command
+    let re = Regex::new(r"^move (?P<count>\d+) from (?P<start>\d+) to (?P<end>\d+)$").unwrap();
+    for line in commands.lines() {
+        dbg!(line);
+        if let Some(caps) = re.captures(line) {
+            let count = caps["count"].parse::<usize>().unwrap();
+            let start = caps["start"].parse::<usize>().unwrap();
+            let end = caps["end"].parse::<usize>().unwrap();
+            dbg!((count, start, end));
+            for _ in 0..count {
+                let moved = stacks[start - 1].pop_front().unwrap();
+                stacks[end - 1].push_front(moved);
+                println!("moving from {} to {}", start, end);
+            }
+            dbg!(&stacks);
+        }
+    }
+    stacks.iter().map(|stack| stack[0]).collect::<String>()
 }
 
 /// passes problem input to solver for the given day
 fn main() -> Result<(), Box<dyn Error>> {
-    let days = [day1, day2, day3, day4, day5];
+    // let days = [day1, day2, day3, day4, day5];
+    let days = [day5, day5, day5, day5, day5];
     let today = 5;
-    let contents = fs::read_to_string(format!("./input/day{}.{}", today, "test"))
+    let contents = fs::read_to_string(format!("./input/day{}.{}", today, "prod"))
         .expect("where's the input file? didn't find it at './input'");
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
