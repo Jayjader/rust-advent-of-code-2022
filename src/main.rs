647ee96f0fdddd2ef7cc12cf743ef4c381571870
Jayjader <jayjader@github.com>
2022-12-09 23:40:25 +0100

    day 7 - extract common logic for both parts

--- src/main.rs
+++ src/main.rs
@@ -491,38 +491,15 @@ fn day7(input: &str, part: Part) -> Solution {
             }
         }
     }
-
-    fn part1(input: &str) -> usize {
-        /// calculates total size of a dir
-        /// stores all dir sizes in cache to avoid duplicate calculations
-        /// also allows inspecting/retrieval of all dir sizes calculated
-        fn total_size<'a>(
-            tree: &HashMap<String, Vec<DirEntry<'a>>>,
-            dir: String,
-            cache: &mut HashMap<String, usize>,
-        ) -> usize {
-            if let Some(cached_size) = cache.get(dir.as_str()) {
-                return *cached_size;
-            }
-            let size = tree
-                .get(dir.as_str())
-                .expect("dir does not exist in tree")
-                .iter()
-                .map(|entry| match entry {
-                    DirEntry::File(_, size) => *size,
-                    DirEntry::Dir(name) => {
-                        let path = [dir.as_str(), *name].join("/");
-                        total_size(tree, path, cache)
-                    }
-                })
-                .sum();
-            cache.insert(dir, size);
-            size
-        }
-
-        let dir_map = &mut HashMap::<String, Vec<DirEntry>>::new();
-        let cwd = &mut Vec::new();
-        let (accu, _) = input
+    fn parse_input_and_build_fs_tree<'a>(
+        input: &'a str,
+        dir_map: &'a mut HashMap<String, Vec<DirEntry<'a>>>,
+        cwd: &'a mut Vec<&'a str>,
+    ) -> (
+        &'a mut HashMap<String, Vec<DirEntry<'a>>>,
+        &'a mut Vec<&'a str>,
+    ) {
+        input
             .split("$ ")
             .skip(1 /*first element of split is empty string*/)
             .map(|command_with_output| {
@@ -558,7 +535,38 @@ fn day7(input: &str, part: Part) -> Solution {
                 };
 
                 (accu, cwd)
-            });
+            })
+    }
+    /// calculates total size of a dir
+    /// stores all dir sizes in cache to avoid duplicate calculations
+    /// also allows inspecting/retrieval of all dir sizes calculated
+    fn total_size<'a>(
+        tree: &HashMap<String, Vec<DirEntry<'a>>>,
+        dir: String,
+        cache: &mut HashMap<String, usize>,
+    ) -> usize {
+        if let Some(cached_size) = cache.get(dir.as_str()) {
+            return *cached_size;
+        }
+        let size = tree
+            .get(dir.as_str())
+            .expect("dir does not exist in tree")
+            .iter()
+            .map(|entry| match entry {
+                DirEntry::File(_, size) => *size,
+                DirEntry::Dir(name) => {
+                    let path = [dir.as_str(), *name].join("/");
+                    total_size(tree, path, cache)
+                }
+            })
+            .sum();
+        cache.insert(dir, size);
+        size
+    }
+    fn part1(input: &str) -> usize {
+        let dir_map = &mut HashMap::<String, Vec<DirEntry>>::new();
+        let cwd = &mut Vec::new();
+        let (accu, _) = parse_input_and_build_fs_tree(input, dir_map, cwd);
 
         let mut dir_sizes = HashMap::new();
 
@@ -569,77 +577,15 @@ fn day7(input: &str, part: Part) -> Solution {
             .filter_map(|(_, v)| if *v <= 100_000 { Some(v) } else { None })
             .sum()
     }
-    fn part2(input: &str) -> usize {
-        /// calculates total size of a dir
-        /// stores all dir sizes in cache to avoid duplicate calculations
-        /// also allows inspecting/retrieval of all dir sizes calculated
-        fn total_size<'a>(
-            tree: &HashMap<String, Vec<DirEntry<'a>>>,
-            dir: String,
-            cache: &mut HashMap<String, usize>,
-        ) -> usize {
-            if let Some(cached_size) = cache.get(dir.as_str()) {
-                return *cached_size;
-            }
-            let size = tree
-                .get(dir.as_str())
-                .expect("dir does not exist in tree")
-                .iter()
-                .map(|entry| match entry {
-                    DirEntry::File(_, size) => *size,
-                    DirEntry::Dir(name) => {
-                        let path = [dir.as_str(), *name].join("/");
-                        total_size(tree, path, cache)
-                    }
-                })
-                .sum();
-            cache.insert(dir, size);
-            size
-        }
 
+    fn part2(input: &str) -> usize {
         let dir_map = &mut HashMap::<String, Vec<DirEntry>>::new();
         let cwd = &mut Vec::new();
-        let (dir_map, _) = input
-            .split("$ ")
-            .skip(1 /*first element of split is empty string*/)
-            .map(|command_with_output| {
-                let (command, result) = command_with_output.split_once('\n').unwrap();
-                let command = Command::try_from(command).unwrap();
-                (
-                    command,
-                    if result.is_empty() {
-                        None
-                    } else {
-                        Some(result)
-                    },
-                )
-            })
-            .fold((dir_map, cwd), |(accu, cwd), (command, result)| {
-                match command {
-                    Command::ChangeDirParent => {
-                        cwd.pop();
-                    }
-                    Command::ChangeDirChild(child) => {
-                        cwd.push(child);
-                    }
-                    Command::List => {
-                        accu.insert(
-                            cwd.join("/"),
-                            result
-                                .unwrap()
-                                .split_terminator('\n')
-                                .flat_map(|single_result| single_result.try_into())
-                                .collect(),
-                        );
-                    }
-                };
-
-                (accu, cwd)
-            });
+        let (dir_map, _) = parse_input_and_build_fs_tree(input, dir_map, cwd);
 
         let mut dir_sizes = HashMap::new();
 
-        total_size(&dir_map, String::from("/"), &mut dir_sizes);
+        total_size(dir_map, String::from("/"), &mut dir_sizes);
 
         let total_space_used: usize = dir_map
             .values()
