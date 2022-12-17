0c26f5989038557590024c18266d4b3baa1ab398
Jayjader <jayjader@github.com>
2022-12-17 17:58:39 +0100

    day 12, part 1 - solution

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate core;
 
-use std::cmp::Ordering;
-use std::collections::{HashMap, HashSet, VecDeque};
+use std::cmp::{Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 use std::iter::Map;
@@ -1289,8 +1289,199 @@ fn day11(input: &str, part: Part) -> Solution {
     }
 }
 
-fn day12(_input: &str, _part: Part) -> Solution {
-    Solution::String(String::from("not implemented"))
+/// solves the problem for day 12
+fn day12(input: &str, part: Part) -> Solution {
+    fn parse_height_map(input: &str) -> HashMap<(isize, isize), (usize, char)> {
+        input
+            .lines()
+            .enumerate()
+            .flat_map(|(y, line)| {
+                line.chars().enumerate().map(move |(x, height_char)| {
+                    (
+                        (x as isize, y as isize),
+                        height_char,
+                        match height_char {
+                            'S' => 1,
+                            'E' => 26,
+                            _ => height_char as usize - 96,
+                        },
+                    )
+                })
+            })
+            .fold(HashMap::new(), |mut map, (coords, char, height)| {
+                map.insert(coords, (height, char));
+                map
+            })
+    }
+    fn part1(input: &str) -> usize {
+        let heights = parse_height_map(input);
+        let start = heights
+            .iter()
+            .find(|(_coords, (_height, char))| *char == 'S')
+            .unwrap();
+        let end = heights
+            .iter()
+            .find(|(_coords, (_height, char))| *char == 'E')
+            .unwrap();
+        println!("start: {:?}; end: {:?}", &start, &end);
+        let mut distance_to_start: HashMap<(isize, isize), (Distance, Option<(isize, isize)>)> =
+            HashMap::with_capacity(heights.len());
+        #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy)]
+        enum Distance {
+            Int(isize),
+            Infinity,
+        }
+        #[derive(Debug, Eq, Clone, Copy)]
+        struct Unvisited {
+            position: (isize, isize),
+            distance: Distance,
+        }
+        impl PartialEq for Unvisited {
+            fn eq(&self, other: &Self) -> bool {
+                self.distance == other.distance
+            }
+        }
+        impl PartialOrd for Unvisited {
+            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                self.distance.partial_cmp(&other.distance)
+            }
+
+            fn lt(&self, other: &Self) -> bool {
+                self.distance.lt(&other.distance)
+            }
+
+            fn le(&self, other: &Self) -> bool {
+                self.distance.le(&other.distance)
+            }
+
+            fn gt(&self, other: &Self) -> bool {
+                self.distance.gt(&other.distance)
+            }
+
+            fn ge(&self, other: &Self) -> bool {
+                self.distance.ge(&other.distance)
+            }
+        }
+        impl Ord for Unvisited {
+            fn cmp(&self, other: &Self) -> Ordering {
+                self.distance.cmp(&other.distance)
+            }
+
+            fn max(self, other: Self) -> Self {
+                if self.distance.max(other.distance) == self.distance {
+                    self
+                } else {
+                    other
+                }
+            }
+
+            fn min(self, other: Self) -> Self {
+                if self.distance.min(other.distance) == self.distance {
+                    self
+                } else {
+                    other
+                }
+            }
+
+            fn clamp(self, min: Self, max: Self) -> Self {
+                Self {
+                    position: self.position,
+                    distance: self.distance.clamp(min.distance, max.distance),
+                }
+            }
+        }
+        let mut to_visit = BinaryHeap::with_capacity(heights.len());
+        for (coords, (_height, char)) in heights.iter() {
+            distance_to_start.insert(
+                *coords,
+                match char {
+                    'S' => {
+                        to_visit.push(Reverse(Unvisited {
+                            position: *coords,
+                            distance: Distance::Int(0),
+                        }));
+                        (Distance::Int(0), None)
+                    }
+                    _ => {
+                        // unvisited.push(Reverse((coords, Distance::Infinity)));
+                        (Distance::Infinity, None)
+                    }
+                },
+            );
+        }
+        while !to_visit.is_empty() {
+            let next_visited = to_visit.pop().unwrap();
+            let (x, y) = next_visited.0.position;
+            let (current_best_dist, _prev) = distance_to_start.get(&(x, y)).unwrap();
+            let distance_coming_from_current_visited = Distance::Int(match current_best_dist {
+                Distance::Infinity => unreachable!(),
+                Distance::Int(d) => d + 1,
+            });
+            let unvisited =
+                HashSet::from_iter(distance_to_start.iter().filter_map(|(coords, distance)| {
+                    if distance.0 == Distance::Infinity {
+                        Some(*coords)
+                    } else {
+                        None
+                    }
+                }));
+            let neighbors =
+                HashSet::<(isize, isize)>::from([(x + 1, y), (x, y + 1), (x, y - 1), (x - 1, y)]);
+            for coords in neighbors.intersection(&unvisited).cloned() {
+                let old_dist = distance_to_start.get(&coords).unwrap().0;
+                let new_dist = if heights.get(&coords).unwrap().0 as isize
+                    - heights.get(&(x, y)).unwrap().0 as isize
+                    <= 1isize
+                {
+                    distance_coming_from_current_visited.min(old_dist)
+                } else {
+                    Distance::Infinity
+                };
+                if new_dist < old_dist {
+                    // println!(
+                    //     "new shortest path to ({},{}) found (dist={:?}, prev=({},{})",
+                    //     coords.0, coords.1, new_dist, x, y
+                    // );
+                    distance_to_start.insert(coords, (new_dist, Some((x, y))));
+                    to_visit.push(Reverse(Unvisited {
+                        position: coords,
+                        distance: new_dist,
+                    }));
+                }
+            }
+        }
+        let mut path_to_end = Vec::new();
+        let mut head_of_path = *end.0;
+        loop {
+            path_to_end.push(head_of_path);
+            if let Some(prev) = distance_to_start.get(&head_of_path).unwrap().1 {
+                head_of_path = prev;
+            } else {
+                break;
+            }
+        }
+        println!("shortest path to start from end: {:?}", path_to_end);
+        assert!(Distance::Int(0) < Distance::Infinity);
+        assert!(Distance::Int(10) < Distance::Int(30));
+        assert!(Distance::Infinity > Distance::Int(0));
+        assert!(
+            Unvisited {
+                position: (0, 0),
+                distance: Distance::Int(0)
+            } < Unvisited {
+                position: (6, 0),
+                distance: Distance::Infinity
+            }
+        );
+        path_to_end.len() - 1
+    }
+    fn part2(_input: &str) -> usize {
+        0
+    }
+    match part {
+        Part::One => Solution::UNumber(part1(input)),
+        Part::Two => Solution::UNumber(part2(input)),
+    }
 }
 
 /// solves the problem for day13
@@ -1450,7 +1641,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let days = [
         day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13,
     ];
-    let today = 13;
+    let today = 12;
     let prod_or_test = "prod";
     let args: Vec<String> = env::args().collect();
     if args.len() == 1 {
