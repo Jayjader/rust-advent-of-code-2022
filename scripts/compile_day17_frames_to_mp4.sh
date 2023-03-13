1316eefc969e949c068e6bfdad1d11e1a0c0682c
Jayjader <Jayjader@users.noreply.github.com>
2023-03-13 09:25:10 +0100

    day 17, part 1 - fix aspect ratio applied in ffmpeg script for visualization

--- scripts/compile_day17_frames_to_mp4.sh
+++ scripts/compile_day17_frames_to_mp4.sh
@@ -2,6 +2,6 @@ ffmpeg -framerate 100 \
   -i output/frame%d.ppm \
   -c:v libx264 \
   -crf 25 \
-  -vf "scale=36:330,format=yuv420p" \
+  -vf "scale=18:660,format=yuv420p" \
   -movflags +faststart \
   output.mp4
